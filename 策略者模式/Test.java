65e217dcb80460d0706d9a940b3c4d51b21d0d7d
melo <411787243@qq.com>
2019-10-22 22:05:00 +0800

    Update Test.java

--- 策略者模式/Test.java
+++ 策略者模式/Test.java
@@ -6,6 +6,12 @@ package demo.策略者模式;
  */
 public class Test {
 
+    /**
+    * “有一个”关系比“是一个”更好
+    * 当你将两个类结合起来使用，如同本例一样，这就是组合
+    * 这种做法和“继承”不同的地方在于，鸭子的行为不是继承来的，而是和适当的行为对象“组合”来的
+    * 设计原则：多用组合，少用继承
+    */
     public static void main(String[] args) {
         Duck greenDuck = new Duck(new FlyWithWing(), new Quack(), "green");
         Duck redDuck = new Duck(new FlyNoWay(), new MuteQuack(), "red");
